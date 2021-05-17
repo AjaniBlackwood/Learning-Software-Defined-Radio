e1120827c0389b131ba3daffbce418f58a43ddb3
Ajani Blackwood <65378020+AjaniBlackwood@users.noreply.github.com>
2021-05-17 14:41:24 -0400

    Add files via upload

---
+++ Symbol Synchronizer/gr-Blackwood_AdaptiveTime/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/ajani/sdr/gr-Blackwood_AdaptiveTime
+# Build directory: /home/ajani/sdr/gr-Blackwood_AdaptiveTime/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("include/Blackwood_AdaptiveTime")
+subdirs("lib")
+subdirs("swig")
+subdirs("python")
+subdirs("grc")
+subdirs("apps")
+subdirs("docs")
